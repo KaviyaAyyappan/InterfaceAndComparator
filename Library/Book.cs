4207163fea69dc5e31d97922a5945621f19a5ffd
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-05-26 15:47:22 +0200

    Interface and comparator

---
+++ Library/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book : IComparable<Book>
+    {
+        //Constructor
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Author = authors;
+        }
+
+        //Properties of the class
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public IReadOnlyList<string> Author { get; private set; }
+
+        //Comparing the book with Icomparable inerface method CompareTo()
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if(result == 0)
+            {
+                result = this.Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+       
+
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Library
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 2002);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
